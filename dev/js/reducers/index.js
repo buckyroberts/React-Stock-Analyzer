9fa4b8c7346e608943ab15b9f46eaaf2098ec6d5
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-11-13 18:33:58 +0800

    Delete dev directory

--- dev/js/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import CompanyReducer from './reducer-company';
-
-const allReducers = combineReducers({
-    companies: CompanyReducer
-});
-
-export default allReducers
